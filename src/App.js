8d24cae75cdad0b7cdac1df86a1d424e6028a9dc
dhruv-developer <160855360+dhruv-developer@users.noreply.github.com>
2024-08-22 06:06:51 +0530

    Depression

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import QueryPage from './components/QueryPage';
-import Translate from './components/Translate';
-import Welcome from './components/Welcome';
+import Header from './components/Header';  // Import Header component
+import QueryPage from './components/QueryPage';  // Import QueryPage component
+import Translate from './components/Translate';  // Import Translate component
+import Welcome from './components/Welcome';  // Import Welcome component
+import JudgeView from './components/JudgeView';  // Import JudgeView component
+import LawyerView from './components/LawyerView';  // Import LawyerView component
 
 const App = () => {
   return (
     <Router>
-      <Header />
+      <Header /> {/* Display the header on all pages */}
       <Routes>
-        <Route path="/" element={
+        {/* Route for the home page */}
+        <Route 
+          path="/" 
+          element={
             <div>
-              <Welcome />
-              <Translate />
+              <Welcome /> {/* Display Welcome component */}
+              <Translate /> {/* Display Translate component */}
             </div>
-        } />
-        <Route path="/query-page" element={<QueryPage />} /> {/* Route for QueryPage */}
+          } 
+        />
+        {/* Route for QueryPage */}
+        <Route path="/query-page" element={<QueryPage />} />
+
+        {/* Route for JudgeView */}
+        <Route path="/judge-view" element={<JudgeView />} />
+
+        {/* Route for LawyerView */}
+        <Route path="/lawyer-view" element={<LawyerView />} />
       </Routes>
     </Router>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
         <a href="/">Home</a>
         <a href="/docai">Doc.AI</a>
         <a href="/available-documents">Available Documents</a>
-
         <a href="/query-page">Query Page</a> {/* Added link to QueryPage */}
-
+        <a href="/judge-view">Judge View</a> {/* Added link to JudgeView */}
+        <a href="/lawyer-view">Lawyer View</a> {/* Added link to LawyerView */}
       </nav>
       <button>Logout</button>
     </header>
